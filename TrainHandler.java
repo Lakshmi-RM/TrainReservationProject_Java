9ce6a1fdea1444f6d27b164642fffb59c74071e9
Lakshmi Ramanathan <rmlak1999@gmail.com>
2021-09-16 09:18:00 -0400

    New features for passenger booking

--- TrainHandler.java
+++ TrainHandler.java
@@ -8,13 +8,43 @@ import java.util.Scanner;
 
 public class TrainHandler{
 
+	static Connection con;
+	static String userName;
+
+	public TrainHandler(Connection con){
+	    this.con=con;
+	}
+
+	protected void finalize(){
+	    try{
+	    	con.close();
+	    }catch(Exception e){
+		System.out.println("Exception : "+e);
+	    }
+	}
+	public void setUN(String userName){
+	    this.userName=userName;
+	}
+	public String getUN(){
+	    return this.userName;
+	}
+
+    public static Statement connectionHandler(){
+	Statement stmt=null;
+	try{	
+	con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/task","postgres","postgres");
+	stmt = con.createStatement();
+	return stmt;
+	}catch(Exception e){
+	System.out.println("The Exception is "+e);
+	}
+	return stmt;
+    }
+
     public static void insertToDB(String sql){
 	try{
-	    Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/task","postgres","postgres");
-	    Statement stmt = con.createStatement();
+	    Statement stmt = connectionHandler();
 	    stmt.executeUpdate(sql);
-	    stmt.close();
-	    con.close();
 	}catch(Exception e){
 	    System.out.println("Exception : "+e);
 	}
@@ -22,33 +52,30 @@ public class TrainHandler{
 
     public String userLoginAndReturnRole()
     {
-	Scanner s=new Scanner(System.in);	
-
-	String username,password;
+	String uN,pW;
+	Scanner s=new Scanner(System.in);
 	System.out.print("\nEnter your username : ");
-    	username=s.nextLine();
+    	uN=s.nextLine();
     	System.out.print("\nEnter your password : ");
-    	password=s.nextLine();
+    	pW=s.nextLine();
 	try{
-	    Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/task","postgres","postgres");
-	    Statement stmt = con.createStatement();
-	    ResultSet rs = stmt.executeQuery( "SELECT * FROM USERLOGIN;" );
-            while(rs.next()){
-	        String userNameFromDB = rs.getString("username");
-		String passWordFromDB = rs.getString("password");
-		String roleFromDB = rs.getString("role");
-		if((username.equals(userNameFromDB))&&(password.equals(passWordFromDB))){
-		    con.close();
-		    return roleFromDB;
-		}
+	    Statement stmt = connectionHandler();
+	    ResultSet rs = stmt.executeQuery( "SELECT ROLE FROM USERLOGIN WHERE USERNAME='"+uN+"' AND PASSWORD='"+pW+"';" );
+	    String role="Invalid";
+	    if(rs.next()){
+		setUN(uN);
+	    	role = rs.getString("ROLE");
 	    }
+            return role;
 	}catch(Exception e){
-	System.out.println("The Exception is "+e);}
-    	return "1";   
+	    System.out.println("The Exception is "+e);
+	}
+    	return "Invalid";   
     }
 
     public void addoperator()
     {
+	try{
 	Scanner s=new Scanner(System.in);	
     	String username,password;
     	System.out.print("\nEnter the username : ");
@@ -58,13 +85,15 @@ public class TrainHandler{
 	String sql = "INSERT INTO USERLOGIN VALUES('"+username+"','"+password+"','Operator');";
 	insertToDB(sql);
 	System.out.print("\nOperator added successfully");
+	}catch(Exception e){
+	    System.out.println("Exception : "+e);
+	}
     }    
 
     private static void displayStationDetails(int trainNumber){
 	try{
-	    Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/task","postgres","postgres");
-	    Statement stmt = con.createStatement();
-	    ResultSet rs = stmt.executeQuery( "SELECT * FROM ROUTEDETAILS;" );
+	    Statement stmt = connectionHandler();
+	    ResultSet rs = stmt.executeQuery( "SELECT * FROM ROUTEDETAILS WHERE TRAIN_NO='"+trainNumber+"';" );
             while(rs.next()){
 		int trainNo = rs.getInt("train_no");
 		String stationName = rs.getString("station_name");
@@ -83,8 +112,7 @@ public class TrainHandler{
     public void displayTrainDetails(){
 	System.out.println("Train No        No Of Tickets   Train Name      Source          Destination     Departure Time  Arrival Time   Station Name    ArrivalTime     DepartureTime");	
 	try{
-	    Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/task","postgres","postgres");
-	    Statement stmt = con.createStatement();
+	    Statement stmt = connectionHandler();
 	    ResultSet rs = stmt.executeQuery( "SELECT * FROM TRAINDETAILS;" );
             while(rs.next()){
 	        int trainNo = rs.getInt("train_no");
@@ -121,15 +149,23 @@ public class TrainHandler{
         System.out.print("\nEnter Arrival Time as hh:mm format : ");
         String arrivalTime=s.nextLine();
 
+	try{
 	String sql = "INSERT INTO TRAINDETAILS VALUES('"+trainNo+"','"+noOfTickets+"','"+trainName+"','"+sourceStation+"','"+destinationStation+"','"+departureTime+"','"+arrivalTime+"');";
 	insertToDB(sql);
 
 	System.out.print("\nEnter the no. of stations (less than 10): ");
         String noOfStations=s.nextLine();
+	
+	int totalStations=Integer.parseInt(noOfStations)+2;
+	String[] stations=new String[totalStations];	
+	stations[0]=sourceStation;
+	stations[totalStations-1]=destinationStation;
+
         for(int i=0;i<Integer.parseInt(noOfStations);i++)
         {
             System.out.printf("\nEnter station name : ");
             String stationName=s.nextLine();
+	    stations[i+1]=stationName;
             System.out.print("\nEnter Arrival Time as hh:mm format : ");
             String arrTime=s.nextLine();
             System.out.print("\nEnter Departure Time as hh:mm format : ");
@@ -137,7 +173,27 @@ public class TrainHandler{
 	    sql = "INSERT INTO ROUTEDETAILS VALUES('"+trainNo+"','"+stationName+"','"+arrTime+"','"+depTime+"');";
 	    insertToDB(sql);	
         }
+
+	for(int i=0;i<totalStations;i++)
+	{
+	    for(int j=i+1;j<totalStations;j++)
+	    {
+		if(j==i+1){
+		    sql = "INSERT INTO TICKETSPLITUPS(TNO,FROMSTATION,TOSTATION,TICKETSAVL,MIDDLESTATION) VALUES('"+trainNo+"','"+stations[i]+"','"+stations[j]+"','"+noOfTickets+"','Nil');";
+	            insertToDB(sql);
+		}
+		for(int k=i;k+1<j;k++)
+		{   
+		    sql = "INSERT INTO TICKETSPLITUPS(TNO,FROMSTATION,TOSTATION,TICKETSAVL,MIDDLESTATION) VALUES('"+trainNo+"','"+stations[i]+"','"+stations[j]+"','"+noOfTickets+"','"+stations[k+1]+"');";
+	    	    insertToDB(sql);
+		}
+	    }
+	}
+
     	System.out.println("Train added successfully");	
+	}catch(Exception e){
+	    System.out.println("Exception : "+e);
+	}
 	
     }
 
@@ -148,9 +204,63 @@ public class TrainHandler{
     	username=s.nextLine();
     	System.out.print("\nEnter the password : ");
     	password=s.nextLine();
+	try{
 	String sql = "INSERT INTO USERLOGIN VALUES('"+username+"','"+password+"','Passenger');";
 	insertToDB(sql);
 	System.out.print("\nPassenger added successfully");
+	}catch(Exception e){
+	    System.out.println("Exception : "+e);
+	}
+    }
+
+    public void bookTickets(){
+	Scanner s = new Scanner(System.in);
+	int trainNo,ticketsReq;
+	String fromSt,toSt;
+	System.out.print("\nEnter the trainNo : ");
+	trainNo=s.nextInt();
+	s.nextLine();
+	System.out.print("\nEnter the From Station : ");
+	fromSt=s.nextLine();
+	System.out.print("\nEnter the To Station : ");
+	toSt = s.nextLine();
+	System.out.print("\nEnter no. of tickets to be booked : ");
+	ticketsReq = s.nextInt();
+	s.nextLine();
+	try{
+	Statement stmt = connectionHandler();
+	ResultSet rs = stmt.executeQuery("SELECT TNO FROM TICKETSPLITUPS WHERE TNO='"+trainNo+"' AND FROMSTATION='"+fromSt+"' AND TOSTATION='"+toSt+"' AND TICKETSAVL>='"+ticketsReq+"';");
+	if(rs.next()){
+	    int rs1 = stmt.executeUpdate("update ticketsplitups set ticketsavl=ticketsavl-'"+ticketsReq+"' where (fromstation='"+fromSt+"' or tostation='"+toSt+"' or middlestation='"+fromSt+"' or middleStation='"+toSt+"') and tno in (SELECT TNO FROM TICKETSPLITUPS WHERE TNO='"+trainNo+"' AND FROMSTATION='"+fromSt+"' AND TOSTATION='"+toSt+"' AND TICKETSAVL>='"+ticketsReq+"');");
+	    String sql = "INSERT INTO USERTICKETDETAILS VALUES('"+getUN()+"','"+trainNo+"','"+fromSt+"','"+toSt+"','"+ticketsReq+"',now());";
+	    insertToDB(sql);
+	    System.out.println("Your ticket has been successfully booked");
+	}
+	else{
+	    System.out.println("You have entered wrong credentials");
+	}
+	}catch(Exception e){
+	    System.out.println("Exception : "+e);
+	}	
+    }    
+
+    public void myTickets()
+    {
+	String uN = getUN();
+	try{
+	    Statement stmt = connectionHandler();
+	    ResultSet rs = stmt.executeQuery("SELECT * FROM USERTICKETDETAILS WHERE USERNAME='"+uN+"';");
+	    System.out.printf("\n%-15s %-15s %-15s %-15s","Train No","From Station","To Station","No Of Tickets");
+	    while(rs.next()){
+		int tNo=rs.getInt("train_no");
+		String fromSt=rs.getString("from_station");
+		String toSt=rs.getString("to_station");
+		int noOfTkts=rs.getInt("no_of_tickets");
+		System.out.printf("\n%-15d %-15s %-15s %-15d",tNo,fromSt,toSt,noOfTkts);
+	    }
+	}catch(Exception e){
+	    System.out.println("Exception : "+e);
+	}
     }
 
 }
--- TrainMain.java
+++ TrainMain.java
@@ -7,9 +7,12 @@ import java.sql.Statement;
 import java.util.Scanner;
 
 public class TrainMain {
+	
+    static Connection con;
+    static TrainHandler th=new TrainHandler(con);	
+
     private static int subMenuAdmin(){
 	Scanner s=new Scanner(System.in);	
-	TrainHandler th=new TrainHandler();	
 	
 	int choice=0;
 	System.out.print("\nWelcome Admin!");
@@ -42,13 +45,12 @@ public class TrainMain {
     private static int subMenuPassenger()
     {
 	Scanner s=new Scanner(System.in);
-	TrainHandler th=new TrainHandler();	
 	
         System.out.print("\nWelcome Passenger");
         int choice=1;
-        while(choice==1)
+        while(choice!=4)
         {
-	    System.out.print("\n1. Display Train\n2. Log Out");
+	    System.out.print("\n\n1. Display Train\n2. Book Tickets\n3. My Tickets\n4. Log Out");
             System.out.print("\nYour choice : ");
             choice=s.nextInt();
             switch(choice)
@@ -57,6 +59,12 @@ public class TrainMain {
                 th.displayTrainDetails();
                 break;
             case 2:
+		th.bookTickets();
+		break;
+	    case 3:
+		th.myTickets();
+		break;
+	    case 4:
                 System.out.print("\nLogging Out...\n");
                 MainMenu(1);
                 break;
@@ -69,8 +77,7 @@ public class TrainMain {
 
     private static int subMenuOperator()
     {
-	Scanner s=new Scanner(System.in);
-	TrainHandler th=new TrainHandler();	
+	Scanner s=new Scanner(System.in);	
 	
         System.out.print("\nWelcome Operator!");
         int choice=1;
@@ -97,8 +104,7 @@ public class TrainMain {
 
     private static int MainMenu(int choice){
 
-	Scanner s=new Scanner(System.in);
-	TrainHandler th=new TrainHandler();	
+	Scanner s=new Scanner(System.in);	
 	
 	while(choice!=3)
         {
@@ -113,6 +119,7 @@ public class TrainMain {
 		break;
             case 2:
                 roleofuser=th.userLoginAndReturnRole();
+		
                 if(roleofuser.equals("Admin"))
                 {
                     subMenuAdmin();
@@ -157,16 +164,20 @@ public class TrainMain {
 	    Connection con = DriverManager.getConnection(jdbcURL,user,pass);
 	   
 	    Statement stmt = con.createStatement();
-	    String sql = "CREATE TABLE IF NOT EXISTS USERLOGIN(USERNAME TEXT PRIMARY KEY NOT NULL ,PASSWORD TEXT NOT NULL , ROLE TEXT NOT NULL)";
+	    String sql = "CREATE TABLE IF NOT EXISTS USERLOGIN(USERNAME TEXT PRIMARY KEY NOT NULL ,PASSWORD TEXT NOT NULL , ROLE TEXT NOT NULL);";
             stmt.executeUpdate(sql);
-	    sql = "CREATE TABLE IF NOT EXISTS TRAINDETAILS(TRAIN_NO INTEGER PRIMARY KEY NOT NULL ,NO_OF_TICKETS INTEGER NOT NULL , TRAIN_NAME TEXT NOT NULL, SOURCE_STATION TEXT NOT NULL, DESTINATION_STATION TEXT NOT NULL, DEPARTURE_TIME TIME NOT NULL, ARRIVAL_TIME TIME NOT NULL)";
+	    sql = "CREATE TABLE IF NOT EXISTS TRAINDETAILS(TRAIN_NO INTEGER PRIMARY KEY NOT NULL ,NO_OF_TICKETS INTEGER NOT NULL , TRAIN_NAME TEXT NOT NULL, SOURCE_STATION TEXT NOT NULL, DESTINATION_STATION TEXT NOT NULL, DEPARTURE_TIME TIME NOT NULL, ARRIVAL_TIME TIME NOT NULL);";
             stmt.executeUpdate(sql);
-	    sql = "CREATE TABLE IF NOT EXISTS ROUTEDETAILS(TRAIN_NO INTEGER NOT NULL ,STATION_NAME TEXT NOT NULL, ARRIVAL_TIME TIME NOT NULL, DEPARTURE_TIME TIME NOT NULL,CONSTRAINT FK FOREIGN KEY(TRAIN_NO) REFERENCES TRAINDETAILS(TRAIN_NO))";
+	    sql = "CREATE TABLE IF NOT EXISTS ROUTEDETAILS(TRAIN_NO INTEGER NOT NULL ,STATION_NAME TEXT NOT NULL, ARRIVAL_TIME TIME NOT NULL, DEPARTURE_TIME TIME NOT NULL,CONSTRAINT FK FOREIGN KEY(TRAIN_NO) REFERENCES TRAINDETAILS(TRAIN_NO) ON DELETE CASCADE);";
+            stmt.executeUpdate(sql);
+	    sql = "CREATE TABLE IF NOT EXISTS USERTICKETDETAILS(USERNAME TEXT, TRAIN_NO INTEGER NOT NULL ,FROM_STATION TEXT NOT NULL, TO_STATION TEXT NOT NULL, NO_OF_TICKETS INTEGER NOT NULL, TIME_OF_BOOKING TIMESTAMP, PRIMARY KEY(USERNAME,TRAIN_NO),CONSTRAINT FK FOREIGN KEY(TRAIN_NO) REFERENCES TRAINDETAILS(TRAIN_NO) ON DELETE CASCADE,CONSTRAINT FK1 FOREIGN KEY(USERNAME) REFERENCES USERLOGIN(USERNAME) ON DELETE CASCADE);";
+            stmt.executeUpdate(sql);
+	    sql = "CREATE TABLE IF NOT EXISTS TICKETSPLITUPS(ID INTEGER, TNO INTEGER NOT NULL ,FROMSTATION TEXT NOT NULL, TOSTATION TEXT NOT NULL, TICKETSAVL INTEGER NOT NULL, MIDDLESTATION TEXT ,PRIMARY KEY(ID), FOREIGN KEY(TNO) REFERENCES TRAINDETAILS(TRAIN_NO) ON DELETE CASCADE);";
             stmt.executeUpdate(sql);
             stmt.close();
 	    con.close();
 	}catch(Exception e){
-	    System.out.println("Exception raised is : "+e);
+	    System.out.println("1Exception raised is : "+e);
 	}
 	
 	MainMenu(1);
